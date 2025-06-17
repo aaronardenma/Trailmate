a0fcaed058527eb36a666f44d4e3d3c97b43c577
Pranjali <pranjalidas2201@gmail.com>
2025-06-17 14:52:14 -0700

    user routes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const trailRoutes = require('./routes/trailRoutes');
+const userRoutes = require('./routes/userRoutes');
+
 
 
 dotenv.config();
@@ -14,6 +16,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/trails', trailRoutes);
+app.use('/api/users', userRoutes);
+
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
