856323c1b411ed9d43c606b82f0f85cbe2210018
lauraqiu <lauraqiu21@gmail.com>
2025-06-20 20:39:05 -0700

    added registration page back

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,34 @@
-import Home from "./pages/Home"
-import Map from "./pages/Map.jsx"
-import Nav from "./components/Nav"
-import { Routes, Route } from "react-router-dom"
-import Filters from "./pages/Filters"
-import Landing from "./pages/Landing.jsx"
-import Favourites from "./pages/Favourites"
-import UserProfile from "./pages/UserProfile"
+import { Routes, Route, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Map from "./pages/Map.jsx";
+import Nav from "./components/Nav";
+import Filters from "./pages/Filters";
+import Landing from "./pages/Landing.jsx";
+import Favourites from "./pages/Favourites";
+import UserProfile from "./pages/UserProfile";
 import TrailPage from "@/pages/TrailPage.jsx";
-import PlanTripPage from "@/pages/PlanTrip.jsx";
-import TripPage from "@/pages/TripPage.jsx";
-import CommunityPage from "@/pages/CommunityPage.jsx";
-import UserPostPage from "@/pages/UserPostPage.jsx";
+import Register from "./pages/Register.jsx";
 
 function App() {
+  const location = useLocation();
+  const noNavPaths = ["/login", "/register", "/landing"];
+  const showNav = !noNavPaths.includes(location.pathname);
 
   return (
-    <>
     <div className="font-display">
-      <Nav className="p-2" />
+      {showNav && <Nav className="p-2" />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="/map" element={<Map />} />
-        <Route path="/trail/:_id" element={<TrailPage />} />
+        <Route path="/trail/:id" element={<TrailPage />} />
         <Route path="/filters" element={<Filters />} />
         <Route path="/favourites" element={<Favourites />} />
         <Route path="/profile" element={<UserProfile />} />
-        <Route path="/planTrip/:_id" element={<PlanTripPage />} />
-        <Route path="/trip/:_id" element={<TripPage />} />
-        <Route path="/community" element={<CommunityPage />} />
-        <Route path="/yourPosts" element={<UserPostPage />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
-      </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/NavGuest.jsx
+++ frontend/src/components/NavGuest.jsx
@@ -4,17 +4,20 @@ export default function NavGuest() {
     return (
         <nav className="flex justify-between items-center bg-[#DAD7CD]">
             <Link to="/landing" className="text-2xl text-[#588157] font-bold p-4" >
-            TrailMate
+                TrailMate
             </Link>
-            <ul className="flex">
+            <ul className="flex items-center">
                 <li className="mr-8">
                     Login
                 </li>
-                <li className="mr-8">
-                    Create account
+                <li>
+                    <Link to="/register">
+                        <button className="mr-8 bg-[#588157] text-white px-4 py-2 rounded hover:bg-[#4a6a43] transition">
+                            Create account
+                        </button>
+                    </Link>
                 </li>
             </ul>
         </nav>
-
     )
 }
