5a1f828fc04c3484f43dfdee19abb6e476fe3b34
Charvi Gulati <charvigulati@hotmail.com>
2025-06-27 22:08:11 -0700

    Added Nodemon and fixed docker

--- backend/models/posts.js
+++ backend/models/posts.js
@@ -4,12 +4,12 @@ const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
     userId: String,
-    title: { type: String, required: true },
-
-    description: { type: String, required: true },
-    dateOfPost: { type: Date, default: Date.now },
-    photoUrl: { type: String, default: '' },
-    likes: { type: Number, default: 0 },
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    dateOfPost: {type: Date, default: Date.now},
+    photoUrl: {type: String, default: ''},
+    likes: {type: Number, default: 0},
+    likedByUsers: [{type: Schema.Types.ObjectId, ref: 'User'}],
     comments: [String]
 }, {
     timestamps: true
